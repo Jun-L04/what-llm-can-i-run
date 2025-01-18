0a4cfc6a4323044340c5864aa44caf9b8a86b881
Junyang Lu <LuJunyang04@gmail.com>
2025-01-18 00:05:45 -0500

    dataframe filtering
    - based on fp16 or bf16

--- whatllm/llm_list.py
+++ whatllm/llm_list.py
@@ -3,6 +3,7 @@ from datasets import load_dataset
 from huggingface_hub import list_models
 import pandas as pd
 import re
+# grateful that pandas supports emojis
 
 # TODO: filter df based on 'fp16' or 'bf16'
 # TODO: check if architecture allows for quantization
@@ -23,20 +24,25 @@ def list_to_excel():
 def get_llm_list(precision: str, capacity: float) -> pd.DataFrame:
     """ Depending on user input, prolly, give us the appropriate list of llms
     """
+    # i'll keep two copies of what is essentially the same cuz i can't think of anything for now
+    precision_str = precision
     precision = float("".join(re.findall(r'\d+', precision))) # parse precision into float
     max_param = precision / 8.0 * capacity # unit memory usage per 1B params * total capacity = max param we can store
     llm_list = pd.DataFrame(columns=['Model Name', 'Average Score', 'Architecture'])
-    param = '#Params (B)'
-
+    precision_mapping ={'fp16': 'float16', 'bf16': 'bfloat16'}
     df = pd.read_excel("leaderboard.xlsx")
-
-    filtered_df = df[df[param] <= max_param]  # filter by model parameters
+    # filter by model parameters, and precision if applicable
+    filtered_df = df[
+        (df['#Params (B)'] <= max_param) &
+        (df['Precision'] == precision_mapping.get(precision_str.lower(), None))
+        ]  # can None be an error or exception that we can return or log?
+    
     sorted_df = filtered_df.sort_values(by='Average ⬆️', ascending=False).reset_index(drop=True)  # sort by average score in descending order and get natural indexing
     #sorted_df.to_excel("sorted_leaderboard.xlsx")
 
     for idx in range(5): # top 5 performing models
         model_name = sorted_df['fullname'][idx]
-        model_average = sorted_df['Average ⬆️'][idx]
+        model_average = sorted_df['Average ⬆️'][idx] 
         model_architecture = sorted_df['Architecture'][idx]
 
         llm_list.loc[len(llm_list)] = [model_name, model_average, model_architecture]
